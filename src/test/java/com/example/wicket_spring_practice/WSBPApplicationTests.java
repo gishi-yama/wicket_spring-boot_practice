24a77030fc06d9b4cf7769c5ef7eafdffcb4a489
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2018-12-10 19:36:38 +0900

    標準テストが動くようにした

--- src/test/java/com/example/wicket_spring_practice/WSBPApplicationTests.java
+++ src/test/java/com/example/wicket_spring_practice/WSBPApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class WicketSpringPracticeApplicationTests {
+@SpringBootTest(classes=com.example.wsbp.WSBPApplication.class)
+public class WSBPApplicationTests {
 
 	@Test
 	public void contextLoads() {
