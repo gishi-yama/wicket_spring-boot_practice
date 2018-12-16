57db345516073da96f2a206613f1d3c5c865a3a5
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2018-12-16 13:03:24 +0900

    C01/01.md の部分まで作成

--- src/main/java/com/example/wsbp/page/UserMakerPage.java
+++ src/main/java/com/example/wsbp/page/UserMakerPage.java
@@ -1,13 +1,28 @@
 package com.example.wsbp.page;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.link.Link;
 import org.wicketstuff.annotation.mount.MountPath;
 
 @MountPath("UserMaker")
 public class UserMakerPage extends WebPage {
 
   public UserMakerPage() {
+    Link<Void> toHomeLink = new BookmarkablePageLink<>("toHome", HomePage.class);
+    add(toHomeLink);
 
+    Form<Void> userInfoForm = new Form<>("userInfo");
+    add(userInfoForm);
+
+    TextField<String> userNameField = new TextField<>("userName");
+    userInfoForm.add(userNameField);
+
+    PasswordTextField userPassField = new PasswordTextField("userPass");
+    userInfoForm.add(userPassField);
   }
 
 }
