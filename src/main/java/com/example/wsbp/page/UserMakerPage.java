7262a734217e1b4aa24047fe345bbb0141107748
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2018-12-17 16:36:54 +0900

    モデルと送信処理までを作成

--- src/main/java/com/example/wsbp/page/UserMakerPage.java
+++ src/main/java/com/example/wsbp/page/UserMakerPage.java
@@ -6,22 +6,36 @@ import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.wicketstuff.annotation.mount.MountPath;
 
 @MountPath("UserMaker")
 public class UserMakerPage extends WebPage {
 
   public UserMakerPage() {
+    IModel<String> userNameModel = Model.of("");
+    IModel<String> userPassModel = Model.of("");
+
     Link<Void> toHomeLink = new BookmarkablePageLink<>("toHome", HomePage.class);
     add(toHomeLink);
 
-    Form<Void> userInfoForm = new Form<>("userInfo");
+    Form<Void> userInfoForm = new Form<Void>("userInfo") {
+      @Override
+      protected void onSubmit() {
+        String msg = "送信データ："
+          + userNameModel.getObject()
+          + ","
+          + userPassModel.getObject();
+        System.out.println(msg);
+      }
+    };
     add(userInfoForm);
 
-    TextField<String> userNameField = new TextField<>("userName");
+    TextField<String> userNameField = new TextField<>("userName", userNameModel);
     userInfoForm.add(userNameField);
 
-    PasswordTextField userPassField = new PasswordTextField("userPass");
+    PasswordTextField userPassField = new PasswordTextField("userPass", userPassModel);
     userInfoForm.add(userPassField);
   }
 
