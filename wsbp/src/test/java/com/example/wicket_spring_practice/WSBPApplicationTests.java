5541c0b4436d7d173f75cc56fe171a17e23b5fe7
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2021-04-01 12:14:31 +0900

    テストコードのアノテーションを実態に合わせる

--- wsbp/src/test/java/com/example/wicket_spring_practice/WSBPApplicationTests.java
+++ wsbp/src/test/java/com/example/wicket_spring_practice/WSBPApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.wicket_spring_practice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = com.example.wsbp.WSBPApplication.class)
+@SpringBootTest
 public class WSBPApplicationTests {
 
   @Test
